d1dc6fba0a19b6c368ee6ed17fdd1afd5c97f4aa
Hoang Duong <kimduong.pt>
2021-09-28 15:07:02 +0700

    Update example.

--- PAVE-Deeplink-ReactNative/App.js
+++ PAVE-Deeplink-ReactNative/App.js
@@ -1,11 +1,18 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {Linking, Platform} from 'react-native';
+import {Linking} from 'react-native';
 
 import {View, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 
 const App = () => {
   const [url, setUrl] = useState('');
+
+  const API_KEY = 'fddb6f33-c882-4042-976c-f10b0abdeee8';
+
+  const REDIRECT_URL = 'pave://';
+
+  const [sessionID, setSessionID] = useState('--');
+  const [redirectUrl, setRedirectUrl] = useState('--');
 
   useEffect(() => {
     Linking.getInitialURL()
@@ -17,12 +24,84 @@ const App = () => {
 
   function handleURL(event) {
     setUrl(event.url);
-    console.log(Platform.OS + ' - url ======> ' + event.url);
+    console.log('url ======> ' + event.url);
   }
+
+  const generateSessionID = () => {
+    var formData = new FormData();
+    formData.append('redirect_url', REDIRECT_URL);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formData,
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://api.paveapi.com/v1/generate-session-id/${API_KEY}`,
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result => {
+        setSessionID(result.session_key);
+        setRedirectUrl(result.redirect_url);
+      })
+      .catch(error => console.log('error', error));
+  };
+
+  const routeToPaveCapture = () => {
+    const PAVE_CAPTURE_PATH = 'https://api.paveapi.com/v1/launch/';
+    Linking.openURL(PAVE_CAPTURE_PATH + sessionID);
+  };
 
   return (
     <View style={{justifyContent: 'center', flex: 1, alignItems: 'center'}}>
       <Text>PAVE DEEP LINK: {url}</Text>
+
+      <View>
+        <TouchableOpacity
+          onPress={generateSessionID}
+          style={[
+            {
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 140,
+              elevation: 4,
+              height: 50,
+              backgroundColor: 'green',
+              borderRadius: 40,
+            },
+          ]}>
+          <Text style={{color: 'white'}}>Create Session</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text>SessionID</Text>
+        <Text>{sessionID}</Text>
+      </View>
+
+      <View>
+        <Text>URL_REDIRECT: {redirectUrl}</Text>
+      </View>
+
+      <View>
+        <TouchableOpacity
+          onPress={routeToPaveCapture}
+          style={[
+            {
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 140,
+              elevation: 4,
+              height: 50,
+              backgroundColor: 'green',
+              borderRadius: 40,
+            },
+          ]}>
+          <Text style={{color: 'white'}}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
